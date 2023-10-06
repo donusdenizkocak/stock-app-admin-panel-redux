2d4aaa67ad930e96056f0ad39bec8d37f277262c
denizderK <denizderya8668@gmail.com>
2023-10-07 00:03:20 +0300

    url

--- src/apiCall/authCall.jsx
+++ src/apiCall/authCall.jsx
@@ -1,6 +1,8 @@
 import axios from "axios"
 
 const login =async () =>{
+    const BASE_URL="https://12338.fullstack.clarusway.com/"
+    const Token="e5b410d1c727fb3ffb9d12deaeea6a37ac1bb56e"
     try {
         const {data} = await axios.post(url, veri)
     } catch (error) {
